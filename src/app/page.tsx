0343fd91d2b339e4e5e202410338e134802f246b
PsychoKillerdd <onlyvicenteuribe@gmail.com>
2025-05-04 22:36:47 -0400

    Componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -7,6 +8,12 @@ export default function Home() {
     <div className="bg-slate-950 text-white text-center p-28 ">
     <h1>Hola Mundo!</h1>
     <div className="miclase">hola</div>
+    <Image
+      src="/pokemongirls.jpg"
+      width={200}
+      height={500}
+      alt="Picture of the author"
+    />
     </div>
     </>
   )
