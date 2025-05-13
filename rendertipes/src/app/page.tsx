b3fbf41e0d387e3e64de957648a3a0fbfffaead9
PsychoKillerdd <onlyvicenteuribe@gmail.com>
2025-05-12 23:29:09 -0400

    React hook

--- rendertipes/src/app/page.tsx
+++ rendertipes/src/app/page.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function page() {
-  return (
-<>
 
+const [count, setCount] = useState(0);
+return (
+<>
 <h1>Hello World</h1>  
+<button onClick={() => setCount(count + 1)}>Click me</button>
+<p>Count: {count}</p>
 </>
 )
 }
