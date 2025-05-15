4f42eb9c58939256805fc13321e05b355ceae8ca
PsychoKillerdd <onlyvicenteuribe@gmail.com>
2025-05-14 23:51:14 -0400

    Rect1

--- rendertipes/src/app/page.tsx
+++ rendertipes/src/app/page.tsx
@@ -2,12 +2,10 @@ import React, { useState } from 'react'
 
 export default function page() {
 
-const [count, setCount] = useState(0);
 return (
 <>
 <h1>Hello World</h1>  
-<button onClick={() => setCount(count + 1)}>Click me</button>
-<p>Count: {count}</p>
+
 </>
 )
 }
