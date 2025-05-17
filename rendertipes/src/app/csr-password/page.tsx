201db5f9835e5e015e842a1607bebd30062b973f
PsychoKillerdd <onlyvicenteuribe@gmail.com>
2025-05-16 23:37:46 -0400

    LAS WEAS

---
+++ rendertipes/src/app/csr-password/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function page() {
+  return (
+<>
+<div className="">
+    <h2>Hola Password</h2>
+</div>
+</>
+
+)
+}
--- rendertipes/src/components/Menu.tsx
+++ rendertipes/src/components/Menu.tsx
@@ -9,7 +9,7 @@ export default function Menu() {
               <Link href="/ssr-landing" className='mb-3  block w-full bg-slate-200 hover:bg-slate-400  p-2 rounded-md'>SSR(Server Side Rendering)</Link>
             </li>
             <li>
-              <Link href="/" className='mb-3  block w-full bg-slate-200 hover:bg-slate-400  p-2 rounded-md'>SSG(Static Site Generation)</Link>
+              <Link href="/csr-password" className='mb-3  block w-full bg-slate-200 hover:bg-slate-400  p-2 rounded-md'>SSG(Static Site Generation)</Link>
             </li>
             <li>
               <Link href="/" className='mb-3 block w-full bg-slate-200 hover:bg-slate-400  p-2 rounded-md'>DR(Dynamic Rendering)</Link>
